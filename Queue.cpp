9bde10b72cd096d5574563075ea631e56217a571
tari2025 <denilestari272@gmail.com>
2025-06-12 11:14:32 +0700

    CEK apakah antrian kosong

--- Queue.cpp
+++ Queue.cpp
@@ -24,3 +24,11 @@ public:
             cout << "\nQueue overflow\n";
             return;
         }
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else
+        {
