acf5dc5eec6f41af3c37791b146d0bbca270a967
tari2025 <denilestari272@gmail.com>
2025-06-12 11:23:34 +0700

    contoh case

--- Queue.cpp
+++ Queue.cpp
@@ -121,3 +121,16 @@ int main()
             cout << "Enter your choice (1-4): ";
             cin >> ch;
             cout << endl;
+
+            switch (ch)
+            {
+            case '1':
+            {
+                q.insert();
+                break;
+            }
+            case '2':
+            {
+                q.remove();
+                break;
+            }
