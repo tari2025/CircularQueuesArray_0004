f0b296e065f953c711fbadddd7ed16bdd3adaa9d
tari2025 <denilestari272@gmail.com>
2025-06-12 11:18:49 +0700

    cek lagi apa ada antrian kosong

--- Queue.cpp
+++ Queue.cpp
@@ -57,3 +57,19 @@ public:
             REAR = -1;
         }
         else
+            // jika element yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+        else FRONT = FRONT + 1;
+    }
+} void display()
+{
+    int FRONT_position = FRONT;
+    int REAR_position = REAR;
+
+    // cek apakah antrian kosong
+    if (FRONT == -1)
+        cout << "Queue is empty\n";
+    return;
+}
+cout << "nElements in the queue are...\n";
