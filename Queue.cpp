8c29f50e21c3c7121d726a90e21af9eafe1a0d52
tari2025 <denilestari272@gmail.com>
2025-06-12 11:16:02 +0700

    jika REAR berada di posisi terakhir array, kembali keawal array

--- Queue.cpp
+++ Queue.cpp
@@ -32,3 +32,13 @@ public:
         }
         else
         {
+            // jika REAR berada di posisi terakhir array, kembali keawal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+    }
+    void remove()
+    {
